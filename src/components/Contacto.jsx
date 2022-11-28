dcba2d4f56a8c1f67c601d74a410286dba559a6c
Lwiggi <luiggi.ruiz.diaz@gmail.com>
2022-11-28 15:55:25 -0300

    28/11.16

---
+++ src/components/Contacto.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function Contacto() {
+  return (
+    <div class="flex justify-center items-center w-screen h-screen bg-white">
+	 <form action="/" method="GET"></form>
+        <div class="container mx-auto my-4 px-4 lg:px-10">
+            <div class="w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
+                <div class="flex">
+                    <h1 class="font-bold text-3xl">Envienos un <br /> mensaje</h1>
+                </div>
+                <div class="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
+                    <input class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                type="text" placeholder="Nombres/s*" />
+                    <input class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                type="text" placeholder="Apellido/s*" />
+                    <input class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                type="email" placeholder="Email*" />
+                    <input class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                type="number" placeholder="Telefono*" />
+            </div>
+				<div class="my-4">
+					<textarea placeholder="Mensaje*" class="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
+				</div>
+				<div class="my-2 w-1/2 lg:w-1/4">
+					<button class="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg
+                      focus:outline-none focus:shadow-outline">
+                     Enviar Mensaje
+                    </button>
+				</div>
+			</div>
+        </div>
+        </form>
+    </div> 
+  );
+}
