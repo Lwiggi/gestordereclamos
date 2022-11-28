81e66514784cd45fdff8cb9a1734691d5956e69a
Lwiggi <luiggi.ruiz.diaz@gmail.com>
2022-11-28 11:35:58 -0300

    28/11.2

--- src/components/NavBar2.jsx
+++ src/components/NavBar2.jsx
@@ -2,14 +2,14 @@ import {Link, NavLink} from 'react-router-dom';
 
 export default function NavBar2() {
   return (
-    <nav>
-        <Link to= '/'></Link>
+   
+        
         <div>
             <NavLink className='nav-link' to='/'>Home</NavLink>
             <NavLink className='nav-link' to='/generarticket'>Nuevo Ticket</NavLink>
             <NavLink className='nav-link' to='/listarticket'>Listar Tickets</NavLink>
             <NavLink className='nav-link' to='/contacto'>Contacto</NavLink>
         </div>
-    </nav>
+    
   )
 }
