0a9e9f7b0c003981e06612ac6fd4b34bfd079aab
Lwiggi <56697383+Lwiggi@users.noreply.github.com>
2022-11-30 10:53:17 -0300

    30/11.navigate

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import  Error  from './Error.jsx';
 
 function Formulario({tickets,setTickets,ticket,setTicket}) {
@@ -42,6 +42,10 @@ function Formulario({tickets,setTickets,ticket,setTicket}) {
   }
 
   const handleSubmit = (e) => {
+    
+    const navigate = useNavigate();
+    
+    navigate('/listarticket');
     e.preventDefault();
 
     // Validacion del formulario
