86990ffa34eccdfc52e6053ad7a758eb5c52df90
Lwiggi <56697383+Lwiggi@users.noreply.github.com>
2022-11-30 12:53:44 -0300

    30/11.undo_navigate

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import  Error  from './Error.jsx';
 
 function Formulario({tickets,setTickets,ticket,setTicket}) {
