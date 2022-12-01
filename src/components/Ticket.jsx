1f462466f5c6c3b146bc1ffa1b68ed88e8b5b397
Lwiggi <luiggi.ruiz.diaz@gmail.com>
2022-11-30 21:17:25 -0300

    30/11.navigate fin

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -4,7 +4,8 @@ import { useNavigate } from "react-router-dom";
 const Ticket = ({ticket,setTicket}) => {
 
   const {cliente,contacto,email,fecha,falla,estado,id} = ticket
-
+  const navigate = useNavigate();
+  navigate("/nvotkt")
 
  /*  const handleEliminar = () => {
      const respuesta = confirm('Deseas eliminar este ticket')
